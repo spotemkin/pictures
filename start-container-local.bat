ca190274dabd07157f3f9a6f933b043919b9932c
Sergey P. <prsl.ru@gmail.com>
2024-05-26 14:19:56 +0300

    before refactor

--- start-container-local.bat
+++ start-container-local.bat
@@ -1,3 +1,4 @@
+:: docker build -t imola.io .
 @echo off
-docker run --env-file .env.local -p 3000:3000 -v d:/vscode-work/pictures/pictures:/app -v d:/autopics:/data/pics/auto -v d:/auto-prv:/data/pics/auto-prv my-nodejs-app
+docker run --env-file .env.local -p 3000:3000 -v d:/vscode-work/pictures/pictures:/app -v d:/autopics:/data/pics/auto -v d:/auto-prv:/data/pics/auto-prv imola.io
 
